bb24e56e7286f24a271427709a46be2e992e0a22
aseelps <aseelpsathar@gmail.com>
2020-12-26 17:14:59 +0530

    script: rebase

--- vince_setup.sh
+++ vince_setup.sh
@@ -1,7 +1,7 @@
 ## starting of script
 echo -e "\e[36mstarting setting up device side repo...."
 echo -e "\e[32mDevice tree.."
-rm -rf device/xiaomi/vince && git clone https://github.com/aseelps/device_xiaomi_vince-11 -b PE device/xiaomi/vince
+rm -rf device/xiaomi/vince && git clone https://github.com/aseelps/device_xiaomi_vince-11 -b DotOS device/xiaomi/vince
 echo -e "\e[32mVendor tree.."
 rm -rf vendor/xiaomi/vince && git clone https://github.com/aseelps/vendor_xiaomi_vince-11 vendor/xiaomi/vince
 echo -e "\e[32mKernel tree.."
@@ -9,7 +9,8 @@ rm -rf kernel/xiaomi/vince && git clone https://github.com/aseelps/kernel_xiaomi
 echo -e "\e[36m..."
 echo -e "\e[36m..."
 echo -e "\e[34mSetting up hals...."
-rm -rf hardware/qcom-caf/wlan &&  git clone https://android.googlesource.com/platform/hardware/qcom/wlan hardware/qcom-caf/wlan
+rm -rf hardware/qcom-caf/wlan && cp -r hardware/qcom/wlan hardware/qcom-caf/wlan
+rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr && git clone https://github.com/AOSP-Common/android_vendor_qcom_opensource_data-ipa-cfg-mgr -b 11.0 vendor/qcom/opensource/data-ipa-cfg-mgr
 echo -e "\e[35mfinishing...."
 echo -e "\e[33mDone."
 
