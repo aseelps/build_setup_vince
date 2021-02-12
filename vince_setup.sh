caa6883f8a544cfdd96d738af75433c6ac4d1ef9
aseelps <aseelpsathar@gmail.com>
2021-02-12 20:32:10 +0530

    Revert "script: changed gcc"
    This reverts commit f1f2364c5bca947d1fba5dece0764b04605fc1ed.

--- vince_setup.sh
+++ vince_setup.sh
@@ -11,7 +11,8 @@ echo -e "\e[36m..."
 echo -e "\e[34mSetting up hals...."
 rm -rf hardware/qcom-caf/wlan && cp -r hardware/qcom/wlan hardware/qcom-caf/wlan
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr && git clone https://github.com/AOSP-Common/android_vendor_qcom_opensource_data-ipa-cfg-mgr -b 11.0 vendor/qcom/opensource/data-ipa-cfg-mgr
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git -b gcc-master prebuilts/gcc/linux-x86/aarch64/aarch64-e
+rm -rf prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 && git clone https://github.com/DerpFest-11/prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth 1 prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
+rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 && git clone https://github.com/DerpFest-11/prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9  --depth 1 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
 echo -e "\e[35mfinishing...."
 echo -e "\e[33mDone."
 
