80416ba34065fbbe0f2298eeb66f9d85261c87ad
aseelps <aseelpsathar@gmail.com>
2021-04-28 19:15:54 +0530

    scrpit: updated hals to r
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- vince_setup.sh
+++ vince_setup.sh
@@ -9,10 +9,11 @@ rm -rf kernel/xiaomi/vince && git clone https://github.com/aseelps/kernel_xiaomi
 echo -e "\e[36m..."
 echo -e "\e[36m..."
 echo -e "\e[34mSetting up hals...."
-rm -rf hardware/qcom-caf/wlan && cp -r hardware/qcom/wlan hardware/qcom-caf/wlan
-rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr && git clone https://github.com/AOSP-Common/android_vendor_qcom_opensource_data-ipa-cfg-mgr -b 11.0 vendor/qcom/opensource/data-ipa-cfg-mgr
-rm -rf prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 && git clone https://github.com/DerpFest-11/prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth 1 prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
-rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 && git clone https://github.com/DerpFest-11/prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9  --depth 1 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+#rm -rf hardware/qcom-caf/wlan && cp -r hardware/qcom/wlan hardware/qcom-caf/wlan
+#rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr && git clone https://github.com/AOSP-Common/android_vendor_qcom_opensource_data-ipa-cfg-mgr -b 11.0 vendor/qcom/opensource/data-ipa-cfg-mgr
+rm -rf hardware/qcom-caf/msm8996/audio &&  git clone https://github.com/aseelps/hardware_qcom_audio_msm8996-vince hardware/qcom-caf/msm8996/audio
+rm -rf hardware/qcom-caf/msm8996/media && git clone https://github.com/aseelps/hardware_qcom_media_msm8996-vince hardware/qcom-caf/msm8996/media
+rm -rf hardware/qcom-caf/msm8996/display &&  git clone https://github.com/aseelps/hardware_qcom_display_msm8996-vince hardware/qcom-caf/msm8996/display
 echo -e "\e[35mfinishing...."
 echo -e "\e[33mDone."
 
